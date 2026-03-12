48daf6db022929469faf859627ca0925196dec93
yoananp <yoananabilahp@gmail.com>
2026-03-12 14:55:28 +0700

    Add files via upload
    SQL file containing the database schema generated from the ERD for the Car Rental system.

---
+++ ERD-Car-Rental-V1.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "customers" (
+  "id" int PRIMARY KEY,
+  "name" varchar(100) NOT NULL,
+  "nik" varchar(20) UNIQUE NOT NULL,
+  "phone_number" varchar(15) NOT NULL
+);
+
+CREATE TABLE "cars" (
+  "id" int PRIMARY KEY,
+  "brand" varchar(50) NOT NULL,
+  "type" "enum(SUV,Sedan,MPV,Hatchback,Crossover,Wagon,Pickup,Coupe,Van,Convertible)" NOT NULL,
+  "transmission" "enum(Manual,Automatic)" NOT NULL,
+  "plate_number" varchar(20) UNIQUE NOT NULL,
+  "price_per_day" int NOT NULL,
+  "available" boolean NOT NULL
+);
+
+CREATE TABLE "bookings" (
+  "id" int PRIMARY KEY,
+  "customer_id" int NOT NULL,
+  "car_id" int NOT NULL,
+  "start_date" date NOT NULL,
+  "end_date" date NOT NULL,
+  "total_price" int NOT NULL,
+  "status" "enum(Pending,Confirmed,Completed,Cancelled)"
+);
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("car_id") REFERENCES "cars" ("id") DEFERRABLE INITIALLY IMMEDIATE;
